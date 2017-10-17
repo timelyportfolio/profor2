4eb1e6eaca862bcfeec45805b961dd7658901aaf
timelyportfolio <kent.russell@timelyportfolio.com>
2017-10-16 21:51:11 -0500

    get study type and comparator codes right

---
+++ src/compfilters.js
@@ -0,0 +1,9 @@
+
+export default function() {
+  var data = {
+    id: 'comparator0',
+    name: 'Comparator',
+    children: [{"code":"ba","name":"Before/after","id":"comparator1"},{"code":"between","name":"Between groups","id":"comparator2"},{"code":"cont","name":"Continuous time scale","id":"comparator3"},{"code":"none","name":"None","id":"comparator4"},{"code":"Other","name":"Other","id":"comparator5"},{"code":"pa","name":"Presence/absence of the exposure","id":"comparator6"},{"code":"punc","name":"Puncturated time scale","id":"comparator7"},{"code":"spat","name":"Spatial comparisons (e.g. distance)","id":"comparator8"}]
+  };
+  return data;
+}
