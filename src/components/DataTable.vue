53f2dbc34074cbfe613af6b2579d4bfd53573b20
timelyportfolio <kent.russell@timelyportfolio.com>
2017-10-26 06:35:01 -0500

    fix bug with table render

--- src/components/DataTable.vue
+++ src/components/DataTable.vue
@@ -12,94 +12,101 @@ export default {
       dt: null
     }
   },
+  methods: {
+    renderTable: function(newFiltered) {
+      var $ = window.$
+      if(newFiltered.data.length && !this.dt) {
+        var config = {
+          data: newFiltered.data,
+          columns: Object.keys(newFiltered.data[0]).map(function(col) {
+            var title = col
+            var render = null
+            if(col === "geo") {
+              title = "Country"
+              render = function(data, type, row, meta) {
+                return data.map(function(d) {
+                  return d["Study_country.x"]
+                })
+                .sort()
+                .join(", ")
+              }
+            }
+            if(col === "outcome") {
+              title = "Outcome"
+              render = function(data, type, row, meta) {
+                return data.map(function(d) {
+                  return d["Out_subtype"]
+                })
+                .sort()
+                .join(", ")
+              }
+            }
+            if(col === "intervention") {
+              title = "Intervention"
+              render = function(data, type, row, meta) {
+                return data.map(function(d) {
+                  return d["Int_type"]
+                })
+                .sort()
+                .join(", ")
+              }
+            }
+            if(col === "habitat") {
+              title = "Habitat"
+              render = function(data, type, row, meta) {
+                return data.map(function(d) {
+                  return d["Biome."]
+                })
+                .sort()
+                .join(", ")
+              }
+            }
+            if(col === "study") {
+              title = "Study"
+            }
+            if(col === "comparator") {
+              title = "Comparator"
+              render = function(data, type, row, meta) {
+                return data.map(function(d) {
+                  return d["Comps_type"]
+                })
+                .sort()
+                .join(", ")
+              }
+            }
+            return {
+              data: col,
+              title: title,
+              render: render
+            }
+          })
+        }
+
+        this.dt = $('table', this.$el).DataTable(config)
+        return
+      }
+      
+      if(this.dt) {
+        var dt = this.dt
+        dt.clear()
+        if(newFiltered.data.length) {
+          dt.rows.add(newFiltered.data)
+          // Draw once all updates are done
+        }
+        dt.draw()
+      }
+    }
+  },
   watch: {
     filtered: {
       handler: function(newFiltered) {
-        var $ = window.$
-        if(newFiltered.data.length && !this.dt) {
-          var config = {
-            data: newFiltered.data,
-            columns: Object.keys(newFiltered.data[0]).map(function(col) {
-              var title = col
-              var render = null
-              if(col === "geo") {
-                title = "Country"
-                render = function(data, type, row, meta) {
-                  return data.map(function(d) {
-                    return d["Study_country.x"]
-                  })
-                  .sort()
-                  .join(", ")
-                }
-              }
-              if(col === "outcome") {
-                title = "Outcome"
-                render = function(data, type, row, meta) {
-                  return data.map(function(d) {
-                    return d["Out_subtype"]
-                  })
-                  .sort()
-                  .join(", ")
-                }
-              }
-              if(col === "intervention") {
-                title = "Intervention"
-                render = function(data, type, row, meta) {
-                  return data.map(function(d) {
-                    return d["Int_type"]
-                  })
-                  .sort()
-                  .join(", ")
-                }
-              }
-              if(col === "habitat") {
-                title = "Habitat"
-                render = function(data, type, row, meta) {
-                  return data.map(function(d) {
-                    return d["Biome."]
-                  })
-                  .sort()
-                  .join(", ")
-                }
-              }
-              if(col === "study") {
-                title = "Study"
-              }
-              if(col === "comparator") {
-                title = "Comparator"
-                render = function(data, type, row, meta) {
-                  return data.map(function(d) {
-                    return d["Comps_type"]
-                  })
-                  .sort()
-                  .join(", ")
-                }
-              }
-              return {
-                data: col,
-                title: title,
-                render: render
-              }
-            })
-          }
-
-          this.dt = $('table', this.$el).DataTable(config)
-          return
-        }
-        
-        if(this.dt) {
-          var dt = this.dt
-          dt.clear()
-          if(newFiltered.data.length) {
-            dt.rows.add(newFiltered.data)
-            // Draw once all updates are done
-          }
-          dt.draw()
-        }
-
+        this.renderTable(newFiltered)
       },
       deep: true
     }
+  },
+  mounted: function() {
+    this.renderTable(this.filtered)
   },
   beforeDestroy: function() {
     if(this.dt) {
