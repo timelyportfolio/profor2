2b11f328fdf69e3aeee7c426bd3f5273b317efcf
timelyportfolio <kent.russell@timelyportfolio.com>
2017-10-31 07:20:00 -0500

    change align of tables

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -1,10 +1,12 @@
 <template>
-  <ul class="nav">
-    <li class="nav-item">
-      <router-link class="nav-link" to="/interactives/charts">Visualization</router-link>
-    </li>
-    <li class="nav-item">
-      <router-link class="nav-link" to="/interactives/tables">Table</router-link>
-    </li>
-  </ul>
+  <nav class="navbar navbar-expand-md fixed-top" style="background-color:#eee;">
+    <ul class="nav">
+      <li class="nav-item">
+        <router-link class="nav-link" to="/interactives/charts">Visualization</router-link>
+      </li>
+      <li class="nav-item">
+        <router-link class="nav-link" to="/interactives/tables">Table</router-link>
+      </li>
+    </ul>
+  </nav>
 </template>
